d5088df823f06e7767f07c7c75734e9326935414
mliashevich <nikita.lyashevich@gmail.com>
2015-11-26 15:08:34 +0300

    Remove extra map operation. Recalculate execution time.

--- Task2/src/main/java/EntryPoint.scala
+++ Task2/src/main/java/EntryPoint.scala
@@ -12,9 +12,9 @@ object EntryPoint {
 
     val file = sc.textFile("hdfs://localhost:8020" + args(0))
 
-    val mapped = file.map(mapper).reduceByKey((a,b)=> a + b)
+    val mapped = file.map(mapper)
 
-    val reduced = mapped.reduceByKey((a,b)=> a + b).sortByKey().map(pair => pair._1 + "\t" + pair._2)
+    val reduced = mapped.reduceByKey((a, b) => a + b).sortByKey().map(pair => pair._1 + "\t" + pair._2)
 
     reduced.saveAsTextFile("hdfs://localhost:8020" + args(1))
   }
@@ -23,7 +23,7 @@ object EntryPoint {
   def mapper(line: String): (Long, Long) = {
     val tokens = line.split("\t")
 
-    val nodeId    = tokens(1).toLong
+    val nodeId = tokens(1).toLong
     val weight = tokens(2).toLong
     (nodeId, weight)
   }
